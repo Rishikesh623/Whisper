9dbdbc7c6230194eedac4eac02ea6a63b4c82c1f
Rishikesh623 <rishikeshkumar39936@gmail.com>
2024-06-05 21:05:49 +0530

    adding socket.io for real time chat

---
+++ socket/index.js
@@ -0,0 +1,45 @@
+const { Server } = require("socket.io");
+
+const io = new Server({
+    cors: {
+        origin: "http://localhost:3000",
+        methods: ["GET", "POST"],
+    },
+});
+
+
+let onlineUsers = [];
+
+io.on("connection", (socket) => {
+    console.log("Socket conn",socket.id);
+
+    //onlne users -- unique socket i d
+    // listen to event/connection from client (fire this eveny from client )
+    socket.on("addNewUser", (userId) => {
+        !onlineUsers.some(user => user.userId === userId)
+        && 
+        onlineUsers.push({
+            userId,
+            socketId: socket.id
+        });
+        console.log("onlineUsers",onlineUsers);
+    
+        io.emit("getOnlineUsers",onlineUsers);//send it to client
+    });
+    //add msg
+    socket.on("sendMessage",(message) => {
+        const user = onlineUsers.find(user => user.userId === message.recipientId );
+
+        if(user){
+            //snd private msg
+            io.to(user.socketId).emit("getMessage",message);
+        }
+    })
+    //update online users array on disconnetct that is when go offline from online 
+    socket.on("disconnect", () => {
+        onlineUsers = onlineUsers.filter(user => user.socketId !== socket.id)
+        io.emit("getOnlineUsers",onlineUsers);//send it to client
+    })
+});
+
+io.listen(4000);
